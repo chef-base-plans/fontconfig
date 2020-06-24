2b088eb63e20d198843a502a25106e48d24bc98d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-24 23:33:20 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/fontconfig_works.rb
+++ controls/fontconfig_works.rb
@@ -7,9 +7,9 @@ control 'core-plans-fontconfig-works' do
   impact 1.0
   title 'Ensure fontconfig works as expected'
   desc '
-  Verify fontconfig by ensuring 
-  (1) its installation directory exists and 
-  (2) that it returns the expected version.
+  Verify fontconfig by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -19,8 +19,8 @@ control 'core-plans-fontconfig-works' do
     its('stderr') { should be_empty }
   end
   
-  command_relative_path = input('command_relative_path', value: 'bin/fc-list')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  command_relative_path = input('command_relative_path', value: 'bin/fontconfig')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
   describe command("#{command_full_path} --version") do
     its('exit_status') { should eq 0 }
